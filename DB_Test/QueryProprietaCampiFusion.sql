4dd006aa46c0201c0d4ef75b6840cdaa4ebdaa53
Mattia Cavina <mattiacavina95@gmail.com>
2025-10-15 11:22:53 +0200

    Correct importo into tablehystory

--- DB_Test/QueryProprietaCampiFusion.sql
+++ DB_Test/QueryProprietaCampiFusion.sql
@@ -1,18 +1,13 @@
 /*Select field with print filter from fusion DB*/
 SELECT cam.cpNome ,prop.propValore ,cod.codCodice
-from [CAMPI] as cam,
-[PROPCOD] as prop,
-[CODICI] as cod
+from CAMPI as cam,
+PROPCOD as prop,
+CODICI as cod
 where cam.cpID = prop.cpID
 and cod.codID = prop.codID
 and cam.cpNome in ('Officina','Preassemblaggio','Sartoria','Prodotto Finito','Spedizioni','Montatori','Ufficio Acquisti');
 
 /*Create first time history table*/
 INSERT into PRINTHISTORY(cpNome,propValore,codCodice,STARTVALUE, ENDVALUE)
-SELECT cam.cpNome ,prop.propValore ,cod.codCodice, today(),date(31/12/2099)
-from [CAMPI] as cam,
-[PROPCOD] as prop,
-[CODICI] as cod
-where cam.cpID = prop.cpID
-and cod.codID = prop.codID
-and cam.cpNome in ('Officina','Preassemblaggio','Sartoria','Prodotto Finito','Spedizioni','Montatori','Ufficio Acquisti');
+SELECT pr.cpNome ,pr.propValore ,pr.codCodice, now(),'2099/12/31'
+from printstate as pr;
